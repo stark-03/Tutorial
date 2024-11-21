92eae1237f0d61028610ae711957d62bf07873c1
Gargi_Gunapala <gargigunapala28@gmail.com>
2024-11-21 20:17:42 +0530

    addition code

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
 print("Hello World")
 print("Hello I am Gargi")
+
+#code for addition of 2 numbers
+a = int(input(("Enter the 1st number:")))
+b = int(input(("Enter the 2nd number:")))
+def addition(a,b):
+    return (a+b)
+result = addition(a,b)
+print("sum of 2 numbers = ", result)
